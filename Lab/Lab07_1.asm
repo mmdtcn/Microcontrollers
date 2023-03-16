029fc0cb235aec35d8981b8e32eb9b5a46baee82
Mohammad Shirazi <112489298+mmdtcn@users.noreply.github.com>
2023-03-16 10:20:45 -0400

    Create Lab07_1.asm

---
+++ Lab/Lab07_1.asm
@@ -0,0 +1,44 @@
+;
+; Lab07_1.asm
+;
+; Created: 3/16/2023 8:59:46 AM
+; Author : shirazi
+;
+
+; Replace with your application code
+.INCLUDE "M328PDEF.inc"
+
+
+.ORG 0
+	JMP BEFORE_MAIN
+.ORG 0x02				//Photo Interruptor 1
+	JMP EXINT0_ISR
+
+
+BEFORE_MAIN:
+LDI R20, HIGH(RAMEND)
+OUT SPH, R20
+LDI R20, LOW(RAMEND)
+OUT SPL, R20
+
+SBI DDRB, 5			;Set the PB5 port as output connected to the LED
+
+CBI DDRD, 2			//Define input for PIND2 	
+SBI PORTD, 2		//Set the pull up resistor for the switch
+
+
+
+LDI R22, (1<<5)     //set R22 with 0b00100000
+
+LDI R20,1<<INT0
+OUT EIMSK, R20
+SEI
+
+HERE: JMP HERE
+
+
+EXINT0_ISR:
+	IN  R21, PORTB
+	EOR R21, R22
+	OUT PORTB, R21
+	RETI
